a65f1f0e35846d83c99111ed2358ec6cce816efe
Rogerio <rrgmax@bol.com.br>
2019-02-12 17:56:08 -0200

    Update store\views.py file

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 # Create your views here.
 
@@ -9,6 +9,10 @@ def catalog(request):
     request.session.set_expiry(0)    
     store_items = Product.objects.all()
     ctx = {'store_items': store_items, 'cart_items': len(cart)}
+    if request.method == "POST":
+        cart.append(int(resquest.POST['obj_id']))
+        return redirect('catalog')
+
     return render (request, "catalog.html", ctx)
 
 
