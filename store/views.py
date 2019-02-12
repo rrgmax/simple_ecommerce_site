580a0187348913eded62b7eeaefe0ff50791042d
Rogerio <rrgmax@bol.com.br>
2019-02-12 09:01:16 -0200

    Update the views.py with catalog function

--- store/views.py
+++ store/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from models import Product
 # Create your views here.
+
+def catalog(request):
+    if 'cart' not in request.session:
+        request.session['cart'] = []
+    cart = request.session['cart']
+    request.session.set_expiry(0)    
+    store_items = Product.objects.all()
+    ctx = {'store_items': store_items}
+    return render (request, "catalog.html", ctx)
+
+
