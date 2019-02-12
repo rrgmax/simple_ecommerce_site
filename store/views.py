59577211ebb96ff9f23b96c5ec9268943b7b9350
Rogerio <rrgmax@bol.com.br>
2019-02-12 18:12:53 -0200

    Define cart function into store/views.py file

--- store/views.py
+++ store/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render, redirect
 from .models import Product
 # Create your views here.
 
+def cartItems(cart):
+    items = []
+    for item in cart:
+        item.append(Product.objects.get(id=item))
+    return items    
+
+def priceCart(cart):
+    cart_items = cartItems(cart)
+    price = 0
+    for item in cart_items:
+        price += item.price
+    return price    
+
 def catalog(request):
     if 'cart' not in request.session:
         request.session['cart'] = []
@@ -9,10 +22,16 @@ def catalog(request):
     request.session.set_expiry(0)    
     store_items = Product.objects.all()
     ctx = {'store_items': store_items, 'cart_items': len(cart)}
+    
     if request.method == "POST":
         cart.append(int(resquest.POST['obj_id']))
         return redirect('catalog')
-
     return render (request, "catalog.html", ctx)
 
+def cart(request):
+    cart = request.session['cart']
+    request.session.set_expiry(0)
+    ctx = {'cart': cart, 'cart_size': len(cart), 'cart_items': cartItems(cart), 
+            'total_price': priceCart(cart)}
+    return render(request, "cart.html", ctx)        
 
