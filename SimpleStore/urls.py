2262cbd9505bb7142c80e6b26b842367003af062
Rogerio <rrgmax@bol.com.br>
2019-02-12 17:42:31 -0200

    Create static folder and set static files in the SimpleStore/urls.py files

--- SimpleStore/urls.py
+++ SimpleStore/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from store import urls as app_urls
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include(app_urls)),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
