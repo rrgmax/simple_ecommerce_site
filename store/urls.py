540d21cbaa212cca3481a45a21a2e1a367c169ae
Rogerio <rrgmax@bol.com.br>
2019-02-13 03:01:58 -0200

    Insert checkou, admin-login and admin-panel paths into store/urls.py

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.catalog, name="catalog" ),
     path('cart/', views.cart, name="cart"),
     path('cart/remove/', views.removefromcart, name = "remove"),
+    path('cart/checkout/', views.checkout, name='checkout'),
+    path('cart/checkout/complete/', views.completeOrder, name='complete_order'),
+    path('admin-login/', views.adminLogin, name='admin_login'),
+    path('admin-panel/', views.adminDashboard, name='admin'),
 ]
